d46b51dbe33496e1751a990da454078a02919309
Mateusz Marczewski <43947266+Sleyn2@users.noreply.github.com>
2023-02-02 18:41:28 +0100

    dodawanie routingu i tabeli

--- BeerHub/src/app/app-routing.module.ts
+++ BeerHub/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BeerListComponent } from './beer-list/beer-list.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent, pathMatch: 'full' },
-
+  { path: 'beer-list', component: BeerListComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
