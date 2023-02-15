c968b90fc62f419074e970962d8025a28c22b6c9
Mateusz Marczewski <43947266+Sleyn2@users.noreply.github.com>
2023-02-15 19:38:40 +0100

    zmiany dla ulubionych piw front

--- BeerHub/src/app/beer-single/beer-single.component.ts
+++ BeerHub/src/app/beer-single/beer-single.component.ts
@@ -3,53 +3,63 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PunkAPIService } from '../shared/services/PunkAPI.service';
 import { BeerSingleModel } from '../shared/models/beerSingle.model';
 import { CookieService } from 'ngx-cookie-service';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-beer-single',
   templateUrl: './beer-single.component.html',
-  styleUrls: ['./beer-single.component.scss']
+  styleUrls: ['./beer-single.component.scss'],
 })
 export class BeerSingleComponent {
+  private id: any;
+  public beer: BeerSingleModel;
+  private cookieName: string = 'LastBeerSite';
+  public user: SocialUser;
+  public loggedIn: boolean;
 
-  private id: any
-  public beer: BeerSingleModel
-  private cookieName: string = 'LastBeerSite'
-
-
-  constructor(private route: ActivatedRoute, private _punkApiService: PunkAPIService, private router: Router, private cookieService: CookieService) {
-    if (this.router.url == "/random-beer") {
+  constructor(
+    private route: ActivatedRoute,
+    private _punkApiService: PunkAPIService,
+    private router: Router,
+    private cookieService: CookieService,
+    private authService: SocialAuthService
+  ) {
+    if (this.router.url == '/random-beer') {
       this.id = Math.floor(Math.random() * 325) + 1;
-      this.cookieService.set(this.cookieName, this.id.toString())
-    }
-    else if (this.router.url == "/last-beer") {
+      this.cookieService.set(this.cookieName, this.id.toString());
+    } else if (this.router.url == '/last-beer') {
       if (this.cookieService.check(this.cookieName) == false) {
-        this.router.navigate([''])
+        this.router.navigate(['']);
+      } else {
+        this.id = this.cookieService.get(this.cookieName);
+        this.cookieService.set(this.cookieName, this.id.toString());
       }
-      else {
-        this.id = this.cookieService.get(this.cookieName)
-        this.cookieService.set(this.cookieName, this.id.toString())
-      }
-    }
-    else {
-      this.route.paramMap.subscribe(paramMap => {
-        this.id = paramMap.get('beerId')
-        this.cookieService.set(this.cookieName, this.id.toString())
-      })
+    } else {
+      this.route.paramMap.subscribe((paramMap) => {
+        this.id = paramMap.get('beerId');
+        this.cookieService.set(this.cookieName, this.id.toString());
+      });
     }
   }
 
   async ngOnInit() {
-    await this.query()
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = user != null;
+    });
+    await this.query();
   }
 
+  addFavourite() {
+    console.log(this.user);
+  }
 
   query() {
     this._punkApiService.getBeer(this.id).subscribe((data) => {
-
       this.beer = data[0];
       if (!this.beer) {
-        this.router.navigate([''])
+        this.router.navigate(['']);
       }
-    })
+    });
   }
 }
